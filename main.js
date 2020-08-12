761476e127f48b97f9a79d37efd5fffd4efe9c5a
Gustavo Boeira <gustavoboeirasilva@gmail.com>
2020-08-12 15:51:55 -0300

    overview, cor

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// js para que as caixas pixelizadas apareçam
+//caixas pixelizadas
 
 let container = document.querySelector('#container');
 function clonaOsQuadradinnhos1(){
@@ -44,15 +44,57 @@ for(i = 0; i < 22; i++){
 	clonaOsQuadradinnhos2();
 }
 
-// js lapis by gabriel 
+//fazer o quadrado pixelizado no overiew
+
+let container2 = document.querySelector("#overview");
+function clonaOsQuadradinnhos3(){
+
+	let i;
+
+	for(i = 0; i < 18; i++){
+		let caixaClonada = document.createElement('DIV');
+		let caixaClonada2 = document.createElement('DIV');
+		container2.appendChild(caixaClonada);
+		container2.appendChild(caixaClonada2);
+		caixaClonada.classList.add('caixaPixelizada2');
+		caixaClonada2.classList.add('caixaPixelizadaPreta2');
+		caixaClonada.classList.add('pixel2');
+		caixaClonada2.classList.add('pixel2');
+	}
+
+}
+
+function clonaOsQuadradinnhos4(){
+
+	let i;
+
+	for(i = 0; i < 18; i++){
+		let caixaClonada = document.createElement('DIV');
+		let caixaClonada2 = document.createElement('DIV');
+		container2.appendChild(caixaClonada2);
+		container2.appendChild(caixaClonada);
+		caixaClonada.classList.add('caixaPixelizada2');
+		caixaClonada2.classList.add('caixaPixelizadaPreta2');
+		caixaClonada.classList.add('pixel2');
+		caixaClonada2.classList.add('pixel2');
+	}
+
+}
+
+for(i = 0; i < 14; i++){
+	clonaOsQuadradinnhos3();
+	clonaOsQuadradinnhos4();
+}
+
+//lapis 
+let index;
 
 function DesenhaLapis(){
-
-
 	let pixels = document.querySelectorAll(".pixel");
 	let container = document.querySelector("#container");
 	let pressed = false;
 	let i;
+	let j;
 
 	container.addEventListener("mousedown", function(){
 		pressed = true;
@@ -62,35 +104,42 @@ function DesenhaLapis(){
 		pressed = false;
 	})
 
-		for(i = 0; i < pixels.length; i++){
-			pixels[i].addEventListener("mouseover", function(){
-				if(pressed){
-					this.style.backgroundColor =  document.getElementById('colorp').value;
-				}
-			});
-		}
+	for(i = 0; i < pixels.length; i++){
+		pixels[i].addEventListener("mouseover", function(){
+			if(pressed){
+				this.style.backgroundColor =  document.getElementById('colorp').value;
+				index = Array.from(container.children).indexOf(this);
+				//alert(index);
+			}
+		});
+	}
+
+	//lapis overview
+	let pixel2 = document.querySelectorAll(".pixel2")
+	Array.from(pixel2);
+	pixel2[index].style.backgroundColor = document.getElementById('colorp').value;
 }
 
-// js borracha by gustavo
+
+//borracha 
 
 function Apaga(){
-
 	let borracha = document.querySelectorAll(".caixaPixelizada");
 	let borracha2 = document.querySelectorAll(".caixaPixelizadaPreta");
 	let i;
 
-		for(i = 0; i < borracha.length; i++){
-			borracha[i].addEventListener("mouseover", function(){
-				this.style.backgroundColor = 'white';
-			});
-		}
-
-		for(i = 0; i < borracha2.length; i++){
-			borracha2[i].addEventListener("mouseover", function(){
-				this.style.backgroundColor = 'rgba(194, 196, 190, 0.9)';
-			});
-		}
+	for(i = 0; i < borracha.length; i++){
+		borracha[i].addEventListener("mouseover", function(){
+			this.style.backgroundColor = 'white';
+		});
 	}
+
+	for(i = 0; i < borracha2.length; i++){
+		borracha2[i].addEventListener("mouseover", function(){
+			this.style.backgroundColor = 'rgba(194, 196, 190, 0.9)';
+		});
+	}
+}
 
 // js para a modal e para escurecer o fundo ao abri-la
 
@@ -121,42 +170,5 @@ function FechaModal(){
 	escuro.style.display = 'none';
 }
 
-//js pra fazer o quadrado pixelizado no overiew
 
-let container2 = document.querySelector("#overview");
-function clonaOsQuadradinnhos3(){
 
-	let i;
-
-	for(i = 0; i < 18; i++){
-		let caixaClonada = document.createElement('DIV');
-		let caixaClonada2 = document.createElement('DIV');
-		container2.appendChild(caixaClonada);
-		container2.appendChild(caixaClonada2);
-		caixaClonada.classList.add('caixaPixelizada2');
-		caixaClonada2.classList.add('caixaPixelizadaPreta2');
-	}
-
-}
-
-function clonaOsQuadradinnhos4(){
-
-	let i;
-
-	for(i = 0; i < 18; i++){
-		let caixaClonada = document.createElement('DIV');
-		let caixaClonada2 = document.createElement('DIV');
-		container2.appendChild(caixaClonada2);
-		container2.appendChild(caixaClonada);
-		caixaClonada.classList.add('caixaPixelizada2');
-		caixaClonada2.classList.add('caixaPixelizadaPreta2');
-	}
-
-}
-
-for(i = 0; i < 14; i++){
-	clonaOsQuadradinnhos3();
-	clonaOsQuadradinnhos4();
-}
-
-// alert(Array.from(container.children).indexOf(this));
